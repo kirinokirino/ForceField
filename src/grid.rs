c40d9a51902d11a7970b958c09681e50913caf33
kirinokirino <kirinokirino2501@gmail.com>
2021-01-15 17:46:48 +0300

    Refactor grid into its own module.

---
+++ src/grid.rs
@@ -0,0 +1,91 @@
+use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
+use sdl2::gfx::primitives::DrawRenderer;
+use sdl2::pixels;
+use sdl2::render::WindowCanvas;
+use simdnoise::NoiseBuilder;
+
+pub struct Grid {
+    cell_size: u32,
+    width: usize,
+    height: usize,
+
+    angles: Vec<f32>,
+    lengths: Vec<f32>,
+
+    seed: i32,
+}
+
+impl Grid {
+    pub fn new(cell_size: u32, seed: i32) -> Self {
+        let width = (SCREEN_WIDTH / cell_size) as usize;
+        let height = (SCREEN_HEIGHT / cell_size) as usize;
+        let seed = seed;
+        Self {
+            cell_size,
+            width,
+            height,
+
+            angles: Grid::flow_field_angles(width, height, 0., 0.),
+            lengths: Grid::flow_field_lengths(width, height, 0., 0.),
+
+            seed,
+        }
+    }
+
+    pub fn draw(&self, canvas: &mut WindowCanvas) {
+        self.draw_grid(canvas);
+        self.fill_grid_with_arc(canvas);
+    }
+
+    fn draw_grid(&self, canvas: &mut WindowCanvas) {
+        let color = pixels::Color::RGB(255, 255, 255);
+        for square in 0..std::cmp::max(SCREEN_HEIGHT, SCREEN_WIDTH) / self.cell_size {
+            let _ = canvas.hline(
+                0,
+                SCREEN_WIDTH as i16,
+                (square * self.cell_size) as i16,
+                color,
+            );
+            let _ = canvas.vline(
+                (square * self.cell_size) as i16,
+                0,
+                SCREEN_HEIGHT as i16,
+                color,
+            );
+        }
+    }
+
+    fn fill_grid_with_arc(&self, canvas: &mut WindowCanvas) {
+        let color = pixels::Color::RGB(255, 255, 255);
+        for y_cell in 0..SCREEN_HEIGHT / self.cell_size {
+            for x_cell in 0..SCREEN_WIDTH / self.cell_size {
+                canvas
+                    .arc(
+                        (x_cell * self.cell_size + self.cell_size / 2) as i16,
+                        (y_cell * self.cell_size + self.cell_size / 2) as i16,
+                        10,
+                        0,
+                        (self.angles[(y_cell * SCREEN_WIDTH / self.cell_size + x_cell) as usize])
+                            as i16,
+                        color,
+                    )
+                    .unwrap();
+            }
+        }
+    }
+
+    fn tick(&mut self) {}
+
+    fn flow_field_angles(width: usize, height: usize, x_offset: f32, y_offset: f32) -> Vec<f32> {
+        NoiseBuilder::gradient_2d_offset(x_offset, width + 1, y_offset, height)
+            .with_seed(0)
+            .with_freq(0.02)
+            .generate_scaled(0.0, 359.9)
+    }
+    fn flow_field_lengths(width: usize, height: usize, x_offset: f32, y_offset: f32) -> Vec<f32> {
+        NoiseBuilder::gradient_2d_offset(x_offset, width + 1, y_offset, height)
+            .with_seed(0)
+            .with_freq(0.02)
+            .generate_scaled(-2.0, 2.0)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,16 @@
 
 extern crate sdl2;
 
+mod grid;
+use grid::Grid;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels;
-use sdl2::render::{Canvas, Texture, WindowCanvas};
 use std::time::Duration;
-
-use sdl2::gfx::primitives::DrawRenderer;
-
-use simdnoise::NoiseBuilder;
 
 const SCREEN_WIDTH: u32 = 1366;
 const SCREEN_HEIGHT: u32 = 768;
-const CELL_SIZE: u32 = 32;
 
 fn find_sdl_gl_driver() -> Option<u32> {
     for (index, item) in sdl2::render::drivers().enumerate() {
@@ -45,8 +42,8 @@ fn main() -> Result<(), String> {
 
     canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
     canvas.clear();
-    draw_grid(&mut canvas, CELL_SIZE);
-    fill_grid_with_arc(&mut canvas, &generate_flow_field(0.0, 0.0), CELL_SIZE);
+    let mut grid = Grid::new(32, 0);
+    grid.draw(&mut canvas);
     canvas.present();
 
     let mut events = sdl_context.event_pump()?;
@@ -65,62 +62,14 @@ fn main() -> Result<(), String> {
                 }
 
                 Event::MouseButtonDown { x, y, .. } => {
-                    draw_axis(&mut canvas, x as i16, y as i16);
-                    canvas.present();
+                    continue;
                 }
 
                 _ => {}
             }
         }
-        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
     }
 
     Ok(())
-}
-
-fn draw_axis(canvas: &mut WindowCanvas, x: i16, y: i16) {
-    let color = pixels::Color::RGB(255, 255, 255);
-    let _ = canvas.hline(0, SCREEN_WIDTH as i16, y as i16, color);
-    let _ = canvas.vline(x as i16, 0, SCREEN_HEIGHT as i16, color);
-}
-
-fn draw_grid(canvas: &mut WindowCanvas, cell_size: u32) {
-    let color = pixels::Color::RGB(255, 255, 255);
-    for square in 0..std::cmp::max(SCREEN_HEIGHT, SCREEN_WIDTH) / cell_size {
-        let _ = canvas.hline(0, SCREEN_WIDTH as i16, (square * cell_size) as i16, color);
-        let _ = canvas.vline((square * cell_size) as i16, 0, SCREEN_HEIGHT as i16, color);
-    }
-}
-
-fn fill_grid_with_arc(canvas: &mut WindowCanvas, flow_field_direction: &[f32], cell_size: u32) {
-    let color = pixels::Color::RGB(255, 255, 255);
-    for y_cell in 0..SCREEN_HEIGHT / cell_size {
-        for x_cell in 0..SCREEN_WIDTH / cell_size {
-            canvas
-                .arc(
-                    (x_cell * cell_size + cell_size / 2) as i16,
-                    (y_cell * cell_size + cell_size / 2) as i16,
-                    10,
-                    0,
-                    (flow_field_direction[(y_cell * SCREEN_WIDTH / cell_size + x_cell) as usize])
-                        as i16,
-                    color,
-                )
-                .unwrap();
-        }
-    }
-}
-
-fn generate_flow_field(x_offset: f32, y_offset: f32) -> Vec<f32> {
-    let res = NoiseBuilder::gradient_2d_offset(
-        x_offset,
-        (SCREEN_WIDTH / CELL_SIZE) as usize + 1,
-        y_offset,
-        (SCREEN_HEIGHT / CELL_SIZE) as usize + 1,
-    )
-    .with_seed(0)
-    .with_freq(0.02)
-    .generate_scaled(0.0, 359.9);
-    println!("{}", res.len());
-    res
 }
