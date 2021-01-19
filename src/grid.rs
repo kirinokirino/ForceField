5604d953b4d84dcc1f7b8633e8baa53e796549de
kirinokirino <kirinokirino2501@gmail.com>
2021-01-19 21:54:07 +0300

    Up to speed with Clippy linting.

--- src/grid.rs
+++ src/grid.rs
@@ -4,13 +4,13 @@ use sdl2::render::WindowCanvas;
 use simdnoise::NoiseBuilder;
 
 pub struct Grid {
-    cell_size: u32,
+    cell_size: u16,
 
-    screen_width: u32,
-    screen_height: u32,
+    screen_width: u16,
+    screen_height: u16,
 
-    width: usize,
-    height: usize,
+    width: u16,
+    height: u16,
 
     angles_offset: (f32, f32),
     lengths_offset: (f32, f32),
@@ -25,9 +25,9 @@ pub struct Grid {
 }
 
 impl Grid {
-    pub fn new(cell_size: u32, seed: i32, screen_width: u32, screen_height: u32) -> Self {
-        let width = ((screen_width / cell_size) + 1) as usize;
-        let height = ((screen_height / cell_size) + 1) as usize;
+    pub fn new(cell_size: u16, seed: i32, screen_width: u16, screen_height: u16) -> Self {
+        let width = (screen_width / cell_size) + 1;
+        let height = (screen_height / cell_size) + 1;
         let seed = seed;
         Self {
             cell_size,
@@ -41,8 +41,8 @@ impl Grid {
             angles_offset_change: (0., 0.),
             lengths_offset_change: (0., 0.),
 
-            angles: Grid::flow_field_angles(seed, width, height, 0., 0.),
-            lengths: Grid::flow_field_lengths(seed, width, height, 0., 0.),
+            angles: Self::flow_field_angles(seed, width, height, 0., 0.),
+            lengths: Self::flow_field_lengths(seed, width, height, 0., 0.),
 
             seed,
         }
@@ -50,27 +50,27 @@ impl Grid {
 
     pub fn update(
         &mut self,
-        cell_size: Option<u32>,
+        cell_size: Option<u16>,
         seed: Option<i32>,
-        screen_width: Option<u32>,
-        screen_height: Option<u32>,
+        screen_width: Option<u16>,
+        screen_height: Option<u16>,
     ) {
         if let Some(new_cell_size) = cell_size {
             self.cell_size = new_cell_size;
-            self.width = ((self.screen_width / self.cell_size) + 1) as usize;
-            self.height = ((self.screen_height / self.cell_size) + 1) as usize;
+            self.width = (self.screen_width / self.cell_size) + 1;
+            self.height = (self.screen_height / self.cell_size) + 1;
         }
         if let Some(new_seed) = seed {
             self.seed = new_seed;
         }
         if let Some(new_screen_width) = screen_width {
-            self.width = (new_screen_width / self.cell_size) as usize;
+            self.width = new_screen_width / self.cell_size;
         }
         if let Some(new_screen_height) = screen_height {
-            self.height = (new_screen_height / self.cell_size) as usize;
+            self.height = new_screen_height / self.cell_size;
         }
-        self.angles = Grid::flow_field_angles(self.seed, self.width, self.height, 0., 0.);
-        self.lengths = Grid::flow_field_lengths(self.seed, self.width, self.height, 0., 0.);
+        self.angles = Self::flow_field_angles(self.seed, self.width, self.height, 0., 0.);
+        self.lengths = Self::flow_field_lengths(self.seed, self.width, self.height, 0., 0.);
     }
 
     pub fn draw(&self, canvas: &mut WindowCanvas) {
@@ -79,32 +79,34 @@ impl Grid {
         self.fill_grid_with_vec(canvas);
     }
 
+    #[allow(clippy::cast_possible_wrap)]
     fn draw_grid(&self, canvas: &mut WindowCanvas) {
         let color = pixels::Color::RGBA(255, 255, 255, 15);
         for square in 0..std::cmp::max(self.height, self.width) {
             let _ = canvas.hline(
                 0,
-                (self.width * self.cell_size as usize) as i16,
-                (square * self.cell_size as usize) as i16,
+                (self.width * self.cell_size) as i16,
+                (square * self.cell_size) as i16,
                 color,
             );
             let _ = canvas.vline(
-                (square * self.cell_size as usize) as i16,
+                (square * self.cell_size) as i16,
                 0,
-                (self.height * self.cell_size as usize) as i16,
+                (self.height * self.cell_size) as i16,
                 color,
             );
         }
     }
 
+    #[allow(clippy::cast_possible_wrap, clippy::cast_possible_truncation)]
     fn _fill_grid_with_arc(&self, canvas: &mut WindowCanvas) {
         let color = pixels::Color::RGB(255, 255, 255);
         for y_cell in 0..self.height {
             for x_cell in 0..self.width {
                 canvas
                     .arc(
-                        (x_cell as u32 * self.cell_size + self.cell_size / 2) as i16,
-                        (y_cell as u32 * self.cell_size + self.cell_size / 2) as i16,
+                        (x_cell * self.cell_size + self.cell_size / 2) as i16,
+                        (y_cell * self.cell_size + self.cell_size / 2) as i16,
                         (self.cell_size / 2) as i16,
                         0,
                         (self.angles[(y_cell * self.width + x_cell) as usize]) as i16,
@@ -114,12 +116,14 @@ impl Grid {
             }
         }
     }
+
+    #[allow(clippy::cast_possible_wrap, clippy::cast_possible_truncation)]
     fn fill_grid_with_vec(&self, canvas: &mut WindowCanvas) {
         let color = pixels::Color::RGBA(255, 255, 255, 100);
         for y_cell in 0..self.height {
             for x_cell in 0..self.width {
-                let center_x = (x_cell as u32 * self.cell_size + self.cell_size / 2) as i16;
-                let center_y = (y_cell as u32 * self.cell_size + self.cell_size / 2) as i16;
+                let center_x = (x_cell * self.cell_size + self.cell_size / 2) as i16;
+                let center_y = (y_cell * self.cell_size + self.cell_size / 2) as i16;
                 let angle = self.angles[(y_cell * self.width + x_cell) as usize];
                 let length = self.lengths[(y_cell * self.width + x_cell) as usize];
 
@@ -129,10 +133,14 @@ impl Grid {
                             center_x,
                             center_y,
                             center_x
-                                + ((angle.to_radians() * 2.).cos() * length * self.cell_size as f32
+                                + ((angle.to_radians() * 2.).cos()
+                                    * length
+                                    * f32::from(self.cell_size)
                                     / 2.) as i16,
                             center_y
-                                + ((angle.to_radians() * 2.).sin() * length * self.cell_size as f32
+                                + ((angle.to_radians() * 2.).sin()
+                                    * length
+                                    * f32::from(self.cell_size)
                                     / 2.) as i16,
                             2,
                             color,
@@ -144,10 +152,14 @@ impl Grid {
                             center_x,
                             center_y,
                             center_x
-                                + ((angle.to_radians() * 2.).cos() * length * self.cell_size as f32
+                                + ((angle.to_radians() * 2.).cos()
+                                    * length
+                                    * f32::from(self.cell_size)
                                     / 2.) as i16,
                             center_y
-                                + ((angle.to_radians() * 2.).sin() * length * self.cell_size as f32
+                                + ((angle.to_radians() * 2.).sin()
+                                    * length
+                                    * f32::from(self.cell_size)
                                     / 2.) as i16,
                             color,
                         )
@@ -157,19 +169,19 @@ impl Grid {
         }
     }
 
-    pub fn tick(&mut self, current_tick: f32) {
+    pub fn tick(&mut self, _current_tick: f32) {
         self.angles_offset.0 += self.angles_offset_change.0;
         self.angles_offset.1 += self.angles_offset_change.1;
         self.lengths_offset.0 += self.lengths_offset_change.0;
         self.lengths_offset.1 += self.lengths_offset_change.1;
-        self.angles = Grid::flow_field_angles(
+        self.angles = Self::flow_field_angles(
             self.seed,
             self.width,
             self.height,
             self.angles_offset.0,
             self.angles_offset.1,
         );
-        self.lengths = Grid::flow_field_lengths(
+        self.lengths = Self::flow_field_lengths(
             self.seed,
             self.width,
             self.height,
@@ -185,24 +197,24 @@ impl Grid {
 
     fn flow_field_angles(
         seed: i32,
-        width: usize,
-        height: usize,
+        width: u16,
+        height: u16,
         x_offset: f32,
         y_offset: f32,
     ) -> Vec<f32> {
-        NoiseBuilder::gradient_2d_offset(x_offset, width, y_offset, height + 1)
+        NoiseBuilder::gradient_2d_offset(x_offset, width.into(), y_offset, (height + 1).into())
             .with_seed(seed)
             .with_freq(0.02)
             .generate_scaled(0.0, 359.9)
     }
     fn flow_field_lengths(
         seed: i32,
-        width: usize,
-        height: usize,
+        width: u16,
+        height: u16,
         x_offset: f32,
         y_offset: f32,
     ) -> Vec<f32> {
-        NoiseBuilder::gradient_2d_offset(x_offset, width, y_offset, height + 1)
+        NoiseBuilder::gradient_2d_offset(x_offset, width.into(), y_offset, (height + 1).into())
             .with_seed(seed - 1000)
             .with_freq(0.02)
             .generate_scaled(-1.0, 1.0)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#![warn(clippy::all)]
+#![deny(clippy::all)]
+#![warn(clippy::nursery, clippy::pedantic, clippy::cargo)]
 
 extern crate sdl2;
 
@@ -10,9 +11,10 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels;
 use std::time::Duration;
 
-const SCREEN_WIDTH: u32 = 1366;
-const SCREEN_HEIGHT: u32 = 768;
+const SCREEN_WIDTH: u16 = 1366;
+const SCREEN_HEIGHT: u16 = 768;
 
+#[allow(clippy::cast_possible_truncation)]
 fn find_sdl_gl_driver() -> Option<u32> {
     for (index, item) in sdl2::render::drivers().enumerate() {
         if item.name == "opengl" {
@@ -26,7 +28,7 @@ fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
-        .window("ForceField", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .window("ForceField", SCREEN_WIDTH.into(), SCREEN_HEIGHT.into())
         .opengl() // this line DOES NOT enable opengl, but allows you to create/get an OpenGL context from your window.
         .fullscreen()
         .build()
@@ -42,8 +44,8 @@ fn main() -> Result<(), String> {
 
     canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
     canvas.clear();
-    let mut seed = 0i32;
-    let mut cell_size = 32;
+    let mut seed: i32 = 0;
+    let mut cell_size: u16 = 32;
     let mut grid = Grid::new(cell_size, seed, SCREEN_WIDTH, SCREEN_HEIGHT);
     grid.draw(&mut canvas);
     canvas.present();
@@ -57,6 +59,7 @@ fn main() -> Result<(), String> {
         canvas.clear();
 
         // INPUT ----
+        #[allow(clippy::collapsible_if)]
         for event in events.poll_iter() {
             match event {
                 Event::Quit { .. } => break 'main,
@@ -76,7 +79,6 @@ fn main() -> Result<(), String> {
                     if keycode == Keycode::Quote {
                         cell_size += 1;
                     } else if keycode == Keycode::Period {
-                        let _some_other_stuff = ();
                         if cell_size >= 5 {
                             cell_size -= 1;
                         }
@@ -117,7 +119,7 @@ fn main() -> Result<(), String> {
 
         canvas.present();
         // Framerate limiter.
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000_u32 / 30));
     }
 
     Ok(())
